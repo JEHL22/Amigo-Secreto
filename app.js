1991a31ff5ffa3af747648dd01231d8ff0531471
JEHL22 <jehuamanl@unac.edu.pe>
2025-01-31 18:24:18 -0500

    Completando la logica

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+let inputAmigo = document.getElementById('amigo');
+let listaAmigos = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
+
+function agregarAmigo() {
+    let nombre = inputAmigo.value;
+    if (nombre === '') {
+        return alert('Debes escribir un nombre');
+    }else{
+        amigos.push(nombre);
+    }
+    mostrarAmigos();
+    inputAmigo.value = '';
+}
+
+function mostrarAmigos() {
+    let lista = listaAmigos;
+    lista.innerHTML = '';
+    amigos.forEach(amigo => {
+        lista.innerHTML += `<li>${amigo}</li>`;
+    });
+}
+
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert('No hay amigos para sortear');
+        return;
+    }
+    let indice = Math.floor(Math.random()*amigos.length);
+    let amigoSorteado = amigos[indice];
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+}
+
+
+
